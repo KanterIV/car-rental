396c4c54aaaa15143e682d43bd74d090168084c9
KanterIV <126972573+KanterIV@users.noreply.github.com>
2024-02-16 23:44:30 +0100

    Updates

--- static/js/648.a114aa62.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcar_rental=self.webpackChunkcar_rental||[]).push([[648],{648:(e,a,r)=>{r.r(a),r.d(a,{default:()=>s});var c=r(496);const s=()=>(0,c.jsx)("div",{children:"Favorite"})}}]);
-//# sourceMappingURL=648.a114aa62.chunk.js.map
