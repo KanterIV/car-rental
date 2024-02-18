c0811775195de51bfe6b6129aa0c7a71889c5ebd
KanterIV <126972573+KanterIV@users.noreply.github.com>
2024-02-18 18:13:15 +0100

    fix load more button
    1. Removed button display when user filters auto

--- src/components/LoadMore/LoadMore.jsx
+++ src/components/LoadMore/LoadMore.jsx
@@ -2,11 +2,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { StyledLoadMore } from "./LoadMore.styled";
 import { getMoreRentalCars } from "../../redux/cars/carsSlice";
-import { selectTotalCars } from "../../redux/cars/carsSelectors";
+import {
+  selectFilterValue,
+  selectTotalCars,
+} from "../../redux/cars/carsSelectors";
 
 const LoadMoreBtn = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const totalCars = useSelector(selectTotalCars);
+  const isFilter = useSelector(selectFilterValue);
   const pageLimit = Math.ceil(totalCars / 12);
   const dispatch = useDispatch();
 
@@ -23,7 +27,7 @@ const LoadMoreBtn = () => {
 
   return (
     <StyledLoadMore
-      className={currentPage >= pageLimit && "hidden"}
+      className={currentPage >= pageLimit || isFilter !== "All" ? "hidden" : ""}
       onClick={() => {
         changePageHelper();
       }}
